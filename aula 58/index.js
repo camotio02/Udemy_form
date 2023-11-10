7ce39df3e35f445189c1819006ea279e5cc9fcc2
Camotio02 <temotio.bernardo@snowmanlabs.com>
2023-11-10 17:58:27 -0300

    add

--- aula 58/index.js
+++ aula 58/index.js
@@ -1,17 +1,45 @@
+function Game() {
+    let meusPontos = 100;
+    const form = document.querySelector('form');
+    const tentativa = form.querySelector('input');
 
-function random(min, max) {
-    const rand = Math.random() * (max - min) + min;
+    function random(min, max) {
+        const rand = Math.floor(Math.random() * (max - min + 1)) + min;
+        return rand;
+    }
+    const sendValue = (e) => {
+        e.preventDefault();
+        if (isNaN(tentativa.value) || !Number.isInteger(Number(tentativa.value))) {
+            alert('Por favor, insira informações corretas.');
+            return;
+        }
+        const send = random(0, 20);
+        verification(send, Number(tentativa.value));
+    };
 
-    return rand
+    const verification = (generatedValue, tentativa) => {
+        const getTagPoints = document.querySelector('.points');
+        if (generatedValue === tentativa) {
+            alert("Parabéns! Você ganhou 1 ponto. Seus pontos agora são: " + (meusPontos + 10));
+            meusPontos += 10;
+            getTagPoints.innerHTML = '';
+            getTagPoints.appendChild(creatTag(meusPontos));
+        } else {
+            alert("Ops! Fica para próxima. O valor correto é: " + generatedValue);
+            console.log("Ops! Pontos diminuíram para: " + (meusPontos - 15));
+            meusPontos -= 15;
+            getTagPoints.innerHTML = '';
+            getTagPoints.appendChild(creatTag(meusPontos));
+        }
+    };
+
+    const creatTag = (value) => {
+        const tag = document.createElement('div');
+        tag.innerHTML = value;
+        return tag;
+    };
+
+    form.addEventListener('submit', sendValue);
 }
-let meusPontos = 0;
-const tentativa = 4;
-let temp;
-let rand = Number(random(0, 6).toFixed(0));
-if (rand === tentativa) {
-    console.log("parabens você ganhou 1 ponto" + "seu pontos passaram para: " +( meusPontos + 10))
-    return;
-} else (
-    console.log("Ops! fica para próxima... hiiiiiii" + "pois o valor é: " + rand),
-    console.log("Ops! pontos cairam meu irmão para: " + (meusPontos - 15))
-)
+
+Game();
