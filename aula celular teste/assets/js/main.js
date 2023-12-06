0c5fa6885a98e3754eccf94001049ac42ea25e8d
Camotio02 <temotio.bernardo@snowmanlabs.com>
2023-12-06 17:43:43 -0300

    add

--- aula celular teste/assets/js/main.js
+++ aula celular teste/assets/js/main.js
@@ -1,7 +1,7 @@
 import daysOfTheWeek from './diasDaSemana.js';
 import piliticaDeUso from './piliticaDeUso.js';
 import celularStatus from './default_mode_infos/infos.js'
-import screenLock from './apps/screenLock/tag.js'
+import screensMain from './screens.js'
 const isScreenLock = localStorage.getItem('isScreenLock') || 'icant'
 const isOn = JSON.parse(localStorage.getItem('informacoesCelular'))
 function celphone() {
@@ -36,7 +36,6 @@ function celphone() {
                 const currentDate = currentTime.toLocaleTimeString('pt-BR', {
                     day: '2-digit',
                     month: '2-digit',
-
                 });
                 daysOfTheWeek.inicia(currentDate, currentTime, this.tagDateShow)
                 this.tagDateTimerShow.innerHTML = `${hourTime}:${minutesTime}`;
@@ -45,26 +44,12 @@ function celphone() {
         },
         get clicks() {
             document.addEventListener('click', (e) => {
-                const display = getComputedStyle(this.screen_ff).display;
-                const letsGoScreenLock = display === 'none' && isScreenLock === 'icant'
-                const isFabric =
-                    this.isOn.neverbeenconnected &&
-                    e.target.id === 'on' &&
-                    display === 'flex';
-                if (isFabric) {
-                    const politicaDeUso = confirm(piliticaDeUso);
-                    if (politicaDeUso) {
-                        const celularInfosString = localStorage.getItem('informacoesCelular');
-                        const celularInfos = JSON.parse(celularInfosString);
-                        celularInfos.on = true;
-                        const celularInfosAtualizadoString = JSON.stringify(celularInfos);
-                        localStorage.setItem('informacoesCelular', celularInfosAtualizadoString);
-                        return checkStatus(this.isOn.on)
-                    }
-                }
-                if (letsGoScreenLock) {
-                    localStorage.setItem('isScreenLock', 'cant')
-                    screenLock.creatScreenLockContent()
+                const screens_main = document.querySelector('.screens-main')
+                if (e.target.id === 'on' && screens_main.style.display === 'none') {
+                    localStorage.setItem('screens', 'lock')
+                    setTimeout(() => {
+                        screensMain.creatScreenContent()
+                    }, 1500);
                 }
             })
         },
@@ -72,30 +57,13 @@ function celphone() {
     }
 }
 const startCelphone = celphone()
-function checkStatus(status) {
-    if (status || isOn?.on) {
-        startCelphone.horas;
-        function disploqueia() {
-            return {
-                displayLock: document.querySelector('.screen-off'),
-                displayOnBotton: document.querySelector('.on'),
-                get on() {
-                    this.displayLock.style.display = 'none',
-                        this.displayOnBotton.style.display = 'none'
-                }
-            }
-        }
-        const on = disploqueia()
-        on.on
-    }
-}
+startCelphone.horas;
+startCelphone.canOnCell;
+startCelphone.clicks;
+
 function handleLocalStorageChange(event) {
-    if (event.key === 'isScreenLock') {
-        console.log('isScreenLock foi alterado:', localStorage.getItem('isScreen'));
-        screenLock.creatScreenLockContent()
+    if (event.key === 'screens') {
+        screensMain.creatScreenContent()
     }
 }
 window.addEventListener('storage', handleLocalStorageChange);
-checkStatus()
-startCelphone.canOnCell;
-startCelphone.clicks;
